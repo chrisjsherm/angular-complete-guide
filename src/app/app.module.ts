f23dae275ab99d8b7b62547d941c2ba1b01d95e5
Chris Sherman <chris.sherman@outlook.com>
2019-02-22 14:43:28 -0500

    Create placeholder components. Create header prototype.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,13 @@ import { SuccessAlertComponent } from './success-alert/success-alert.component';
 import { FormsModule } from '@angular/forms';
 import { CreateUserComponent } from './create-user/create-user.component';
 import { DisplayDetailsComponent } from './display-details/display-details.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
+import { RecipeListComponent } from './recipe-book/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipe-book/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipe-book/recipe-detail/recipe-detail.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipe-book/recipes/recipes.component';
 
 @NgModule({
   declarations: [
@@ -19,6 +26,13 @@ import { DisplayDetailsComponent } from './display-details/display-details.compo
     SuccessAlertComponent,
     CreateUserComponent,
     DisplayDetailsComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    HeaderComponent,
+    RecipesComponent,
   ],
   imports: [
     BrowserModule,

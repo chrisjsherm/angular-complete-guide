39de91efc3fd4bf3a024b2ac3885f443456e832f
Chris Sherman <chris.sherman@outlook.com>
2019-02-23 09:38:36 -0500

    Rough-out the application components.

--- src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../models/recipe';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,8 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  words = 'Chris';
+  recipes: Recipe[] = [new Recipe(
+    'A test recipe',
+    'this is simply a test',
+    'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg')
+  ];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }

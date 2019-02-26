83c00f4e5dd1f7056ba9f5388bc026a03d4c8494
Chris Sherman <chris.sherman@outlook.com>
2019-02-26 09:29:41 -0500

    Implement add ingredients.

--- src/app/recipe-book/recipe-detail/recipe-detail.component.ts
+++ src/app/recipe-book/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Recipe } from '../models/recipe';
+import { Ingredient } from 'src/app/shared/models/ingredient';
+import { ShoppingListService } from 'src/app/shopping-list/shopping-list.service';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -9,9 +11,12 @@ import { Recipe } from '../models/recipe';
 export class RecipeDetailComponent implements OnInit {
   @Input() recipe: Recipe;
 
-  constructor() { }
+  constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnInit() {
   }
 
+  onAddIngredients(ingredients: Ingredient[]) {
+    this.shoppingListService.addIngredients(ingredients);
+  }
 }

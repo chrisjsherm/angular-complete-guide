c33f5c76c18962d3735698dc248b27cf9daea470
Chris Sherman <chris.sherman@outlook.com>
2019-03-11 10:35:34 -0400

    Ensure ingredients property exists even if NoSQL DB omits it.

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { RecipeBookService } from '../receipe-book/recipe-book.service';
 import { Recipe } from '../recipe-book/models/recipe';
-
+import { map } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
@@ -23,11 +23,24 @@ export class DataStorageService {
   getRecipes() {
     return this.http.get(
       this.DATABASE_URL
-    ).subscribe(
-      (response: Response) => {
-        const recipes: Recipe[] = response.json();
-        this.recipeService.setRecipes(recipes);
-      }
-    );
+    ).pipe(
+      map(
+        (response: Response) => {
+          const recipes: Recipe[] = response.json();
+          for (const recipe of recipes) {
+            if (!recipe.hasOwnProperty('ingredients')) {
+              recipe['ingredients'] = [];
+            }
+          }
+
+          return recipes;
+        }
+      ))
+      .subscribe(
+        (recipes: Recipe[]) => {
+          console.log(recipes);
+          this.recipeService.setRecipes(recipes);
+        }
+      );
   }
 }

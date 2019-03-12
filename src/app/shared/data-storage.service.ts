fcf906806c0f2f84c247427de8d805d7c8c07739
Chris Sherman <chris.sherman@outlook.com>
2019-03-12 15:44:11 -0400

    Replace Http with HttpClient.

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
+import { Response } from '@angular/http';
 import { RecipeBookService } from '../recipes/recipe-book.service';
 import { Recipe } from '../recipes/models/recipe';
 import { map } from 'rxjs/operators';
 import { AuthService } from '../auth/auth.service';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +13,7 @@ export class DataStorageService {
   readonly DATABASE_URL = 'https://schnitzel-f436e.firebaseio.com/recipes.json';
 
   constructor(
-    private http: Http,
+    private http: HttpClient,
     private recipeService: RecipeBookService,
     private authService: AuthService,
   ) { }
@@ -29,12 +30,11 @@ export class DataStorageService {
   getRecipes() {
     const token = this.authService.getToken();
 
-    return this.http.get(
+    return this.http.get<Recipe[]>(
       this.DATABASE_URL + '?auth=' + token
     ).pipe(
       map(
-        (response: Response) => {
-          const recipes: Recipe[] = response.json();
+        (recipes) => {
           for (const recipe of recipes) {
             if (!recipe.hasOwnProperty('ingredients')) {
               recipe['ingredients'] = [];

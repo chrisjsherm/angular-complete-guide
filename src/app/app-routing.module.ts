f33f58f14eff626d1089a78fc82e288dd8ac1793
Chris Sherman <chris.sherman@outlook.com>
2019-03-12 09:49:41 -0400

    Create auth routing module.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ShoppingListComponent } from './shopping-list/shopping-list/shopping-list.component';
-import { SignupComponent } from './auth/signup/signup.component';
-import { SigninComponent } from './auth/signin/signin.component';
 const appRoutes: Routes = [
   {
     path: '', redirectTo: '/recipes', pathMatch: 'full',
@@ -10,12 +8,6 @@ const appRoutes: Routes = [
   {
     path: 'shopping-list', component: ShoppingListComponent,
   },
-  {
-    path: 'signup', component: SignupComponent
-  },
-  {
-    path: 'signin', component: SigninComponent,
-  }
 ];
 
 @NgModule({
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,6 +3,7 @@ import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { AuthRoutingModule } from './auth-routing.module';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import { FormsModule } from '@angular/forms';
   imports: [
     CommonModule,
     FormsModule,
+    AuthRoutingModule,
   ],
 })
 export class AuthModule { }
